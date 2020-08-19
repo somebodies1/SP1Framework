bb75fdaa58827f399ce9c6a1fc237c0c90ad1bc1
x3adam <waltonong@gmail.com>
2020-08-19 11:41:51 +0800

    add mobs and others

---
+++ SP1Framework/Boss.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Entity.h"
+class Boss :
+	public Entity
+{
+private:
+	int hp, mp;
+public:
+	Boss();
+	~Boss();
+	void spawnBoss(int h, int m);
+	void setHP(int h);
+	void setMP(int m);
+
+	int getHP(void);
+	int getMP(void);
+};
+
