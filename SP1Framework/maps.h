b24646fbce25b1245ab48e5b5130d6accb93c3f8
JoelPangJD <JoelPangjd@gmail.com>
2020-08-21 17:55:43 +0800

    moved tutorial stages to text file

--- SP1Framework/maps.h
+++ SP1Framework/maps.h
@@ -5,18 +5,20 @@ using namespace std;
 class maps
 {
 private:
-	static char stage1_1[25][80];
-	static char stage1_2[25][80];
-	static char stage1_3[25][80];
+	char stage0[3][25][80];
+	char stage1[3][25][80];
 	static char tut_1[25][80];
 	static char tut_2[25][80];
 	static char tut_3[25][80];
+	char stage[10][25][80];
 	char current[25][80];
 	int mapno;
+	int stageno;
 public:
 	maps();
 	~maps();
 	void setcurrent(int mapnum);
+	void setstage(int stagenum);
 	char getchar(int x, int y); //Gets the character in the index
 	void setchar(char add, int x, int y); //Sets the character in the index
 	int getmapno();
